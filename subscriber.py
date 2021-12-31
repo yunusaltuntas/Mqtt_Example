b943d9b1d016c1d9d6a607512d7611c6d51cb5f6
yunusaltuntas <altuntasyunus50@gmail.com>
2021-12-31 15:30:35 +0300

    code refactoring

--- subscriber.py
+++ subscriber.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt
 
-broker = "localhost"
+broker = "mqtt"
 # port
 port = 1883
 # time to live
@@ -8,12 +8,12 @@ time_live = 60
 
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    print("Connected with result code " + str(rc), flush=True)
     client.subscribe("/data")
 
 
 def on_message(client, userdata, msg):
-    print(msg.payload.decode())
+    print(msg.payload.decode(), flush=True)
 
 
 client = mqtt.Client()
