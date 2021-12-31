f90924b6ea186679fa841b8ab2f9b2a96f7fc736
yunusaltuntas <altuntasyunus50@gmail.com>
2021-12-31 14:23:34 +0300

    publisher

---
+++ publiser.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as paho
+import time
+import random
+
+# hostname
+broker = "localhost"
+# port
+port = 1883
+
+
+def on_publish(client, userdata, result):
+    print("Device 1 : Data published.")
+    pass
+
+
+client = paho.Client("admin")
+client.on_publish = on_publish
+client.connect(broker, port)
+global d
+for i in range(20):
+    d = random.randint(1, 5)
+
+    # telemetry to send
+    message = "Device 1 : Data " + str(i)
+time.sleep(d)
+
+# publish message
+ret = client.publish("/data", "message")
+print("Stopped...")
